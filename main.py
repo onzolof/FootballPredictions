175c0ec60b2b8624b9ce36aff7f0b268283bd9a3
onzolof <onzolof@mailfence.com>
2023-04-06 14:57:27 +0200

    new: main file - first draft

---
+++ main.py
@@ -0,0 +1,32 @@
+import logging
+from datetime import datetime
+
+from selenium.webdriver.chrome import webdriver
+
+
+def run():
+    today = datetime.date.today().day
+    logger = _get_logger()
+    driver = _get_driver()
+    driver.close()
+
+
+def _get_logger():
+    logging.basicConfig(filename="advent_bot.log",
+                        filemode='a',
+                        format='%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s',
+                        datefmt='%H:%M:%S',
+                        level=logging.DEBUG)
+    logger: logging.Logger = logging.getLogger('transfermarkt_scraper')
+    return logger
+
+
+def _get_driver():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(30)
+    driver.maximize_window()
+    return driver
+
+
+if __name__ == '__main__':
+    run()
