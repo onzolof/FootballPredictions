dc10a35c1a7fc9f2ca1e5421fd36c410853e16bf
onzolof <onzolof@mailfence.com>
2023-04-09 19:06:57 +0200

    new: error-logging functionality

--- main.py
+++ main.py
@@ -21,6 +21,18 @@ def run():
     driver.close()
 
 
+def do_scraping(driver, logger):
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    for league_link in leagues:
+        for club_link in get_clubs_of_league(driver, league_link):
+            for player_link in get_players_of_club(driver, club_link):
+                try:
+                    player = scrape_player(driver, player_link)
+                    write_player(timestamp, player)
+                except:
+                    log_scraping_error(timestamp, player_link)
+
+
 def scrape_player(driver, player_link):
     player = dict()
     driver.get(player_link)
@@ -29,26 +41,6 @@ def scrape_player(driver, player_link):
     player['no'] = no[1:]
     player['name'] = name
     return player
-
-
-def do_scraping(driver, logger):
-    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-    for league_link in leagues:
-        for club_link in get_clubs_of_league(driver, league_link):
-            for player_link in get_players_of_club(driver, club_link):
-                player = scrape_player(driver, player_link)
-                write_player(timestamp, player)
-
-
-def write_player(timestamp, player):
-    print(f"Scraped:\t\t{player['name']}")
-    filepath = f"data/players_{timestamp}.csv"
-    exists = os.path.exists(filepath)
-    with open(filepath, 'a') as f:
-        w = csv.DictWriter(f, player.keys())
-        if not exists:
-            w.writeheader()
-        w.writerow(player)
 
 
 def get_players_of_club(driver, club_link):
@@ -63,6 +55,24 @@ def get_clubs_of_league(driver, league_link):
     clubs = driver.find_elements(By.XPATH, "//div[@id='yw1']/table/tbody//tr/td[3]/a")
     clubs = list(map(lambda c: c.get_attribute("href"), clubs))
     return clubs
+
+
+def write_player(timestamp, player):
+    print(f"Scraped:\t\t{player['name']}")
+    filepath = f"data/players_{timestamp}.csv"
+    exists = os.path.exists(filepath)
+    with open(filepath, 'a') as f:
+        w = csv.DictWriter(f, player.keys())
+        if not exists:
+            w.writeheader()
+        w.writerow(player)
+
+
+def log_scraping_error(timestamp, player_link):
+    print(f"Error:\t\t{player_link}")
+    filepath = f"data/errors_{timestamp}.csv"
+    with open(filepath, 'a') as f:
+        f.write(player_link)
 
 
 def get_logger():
