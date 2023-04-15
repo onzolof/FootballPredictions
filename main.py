6727ac2afadf1515a7b5a85b89119043307fe115
onzolof <onzolof@mailfence.com>
2023-04-15 21:06:13 +0200

    fix: introduce increment value

--- main.py
+++ main.py
@@ -29,16 +29,17 @@ def do_scraping(driver):
         for club_link in clubs:
             for player_link in get_players_of_club(driver, club_link):
                 try:
-                    player = scrape_player(driver, player_link)
+                    player = dict()
                     player['league_country'] = league_country
-                    player['scraping_time'] = timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+                    player['scraping_time'] = datetime.now().strftime('%Y%m%d%H%M%S')
+                    player['link'] = player_link
+                    player = scrape_player(driver, player_link, player)
                     write_player(timestamp, player)
                 except BaseException as exception:
                     log_scraping_error(timestamp, player_link, exception)
 
 
-def scrape_player(driver, player_link):
-    player = dict()
+def scrape_player(driver, player_link, player):
     lookup = HtmlLookup(driver)
 
     driver.get(player_link)
@@ -51,80 +52,88 @@ def scrape_player(driver, player_link):
     player['league'] = lookup.from_text_by_class('data-header__league')
     player['club'] = lookup.from_text_by_class('data-header__club')
 
-    player['league_level'] = lookup.from_text("//main/header/div[3]/div/span[3]/span")
-    player['club_since'] = lookup.from_text("//main/header//div[3]//div//span[4]/span")
-    player['contract_until'] = lookup.from_text("//main/header//div[3]//div//span[5]/span")
+    increment = 0
+    for index in range(2, 10):
+        value = lookup.from_text(f"//main/header/div[{index}]/div/span[3]/span")
+        if value:
+            player['league_level'] = value
+            increment = index - 2
+            break
 
-    player['age'] = lookup.from_inner_text("//div[@class='row']/div/div[2]/div/div[2]/div/span[6]")
-    player['height'] = lookup.from_inner_text("//div[@class='row']/div/div[2]/div/div[2]/div/span[8]")
-    player['nationality'] = lookup.from_inner_text("//div[@class='row']/div/div[2]/div/div[2]/div/span[10]")
-    player['position'] = lookup.from_inner_text("//div[@class='row']/div/div[2]/div/div[2]/div/span[12]")
-    player['foot'] = lookup.from_inner_text("//div[@class='row']/div/div[2]/div/div[2]/div/span[14]")
-    player['consultancy'] = lookup.from_inner_text("//div[@class='row']/div/div[2]/div/div[2]/div/span[16]/a")
-    player['supplier'] = lookup.from_inner_text("//div[@class='row']/div/div[2]/div/div[2]/div/span[26]")
+    # todo: print just for testing
+    print(increment)
 
-    player['international'] = lookup.from_inner_text('//*[@id="main"]/main/header/div[5]/div/ul[3]/li[1]/span/a')
-    player['international_games'] = lookup.from_inner_text('//*[@id="main"]/main/header/div[5]/div/ul[3]/li[2]/a[1]')
-    player['international_goals'] = lookup.from_inner_text('//*[@id="main"]/main/header/div[5]/div/ul[3]/li[2]/a[2]')
+    player['club_since'] = lookup.from_text(f"//main/header/div[{2 + increment}]/div/span[4]/span")
+    player['contract_until'] = lookup.from_text(f"//main/header/div[{2 + increment}]/div/span[5]/span")
 
-    player['market_value'] = lookup.from_inner_text('//*[@id="main"]/main/header/div[6]/a')
-    player['market_value_currency'] = lookup.from_inner_text('//*[@id="main"]/main/header/div[6]/a/span')
-    player['market_value_latest_correction'] = lookup.from_inner_text('//*[@id="main"]/main/header/div[6]/a/p')
-    player['highest_market_value'] = lookup.from_inner_text(
-        '//*[@id="main"]/main/div[3]/div[1]/div[2]/div/div[1]/div/div[2]/div[2]/div[1]/div[2]/div[2]')
-    player['highest_market_value_date'] = lookup.from_inner_text(
-        '//*[@id="main"]/main/div[3]/div[1]/div[2]/div/div[1]/div/div[2]/div[2]/div[1]/div[2]/div[3]')
+    # todo: fix ff and make it work for all players with all increments
+    player['age'] = lookup.from_inner_text(f"//div[@class='row']/div/div[2]/div/div[2]/div/span[6]")
+    # player['height'] = lookup.from_inner_text("//div[@class='row']/div/div[2]/div/div[2]/div/span[8]")
+    # player['nationality'] = lookup.from_inner_text("//div[@class='row']/div/div[2]/div/div[2]/div/span[10]")
+    # player['position'] = lookup.from_inner_text("//div[@class='row']/div/div[2]/div/div[2]/div/span[12]")
+    # player['foot'] = lookup.from_inner_text("//div[@class='row']/div/div[2]/div/div[2]/div/span[14]")
+    # player['consultancy'] = lookup.from_inner_text("//div[@class='row']/div/div[2]/div/div[2]/div/span[16]/a")
+    # player['supplier'] = lookup.from_inner_text("//div[@class='row']/div/div[2]/div/div[2]/div/span[26]")
+    #
+    # player['international'] = lookup.from_inner_text('//*[@id="main"]/main/header/div[5]/div/ul[3]/li[1]/span/a')
+    # player['international_games'] = lookup.from_inner_text('//*[@id="main"]/main/header/div[5]/div/ul[3]/li[2]/a[1]')
+    # player['international_goals'] = lookup.from_inner_text('//*[@id="main"]/main/header/div[5]/div/ul[3]/li[2]/a[2]')
+    #
+    # player['market_value'] = lookup.from_inner_text('//*[@id="main"]/main/header/div[6]/a')
+    # player['market_value_currency'] = lookup.from_inner_text('//*[@id="main"]/main/header/div[6]/a/span')
+    # player['market_value_latest_correction'] = lookup.from_inner_text('//*[@id="main"]/main/header/div[6]/a/p')
+    # player['highest_market_value'] = lookup.from_inner_text(
+    #     '//*[@id="main"]/main/div[3]/div[1]/div[2]/div/div[1]/div/div[2]/div[2]/div[1]/div[2]/div[2]')
+    # player['highest_market_value_date'] = lookup.from_inner_text(
+    #     '//*[@id="main"]/main/div[3]/div[1]/div[2]/div/div[1]/div/div[2]/div[2]/div[1]/div[2]/div[3]')
+    #
+    # player['games'] = lookup.from_inner_text(
+    #     '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[1]/li[1]/a')
+    # player['yellow_cards'] = lookup.from_inner_text(
+    #     '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[2]/li[1]/a')
+    # player['yellow_red_cards'] = lookup.from_inner_text(
+    #     '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[2]/li[2]/a')
+    # player['red_cards'] = lookup.from_inner_text(
+    #     '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[2]/li[3]/a')
+    #
+    # player['starting_eleven'] = lookup.from_inner_text(
+    #     '//div[@class="tm-player-performance__statistic-gauges svelte-1jbxbl0"]//div[1]/div//span[1]')
+    # player['minutes'] = lookup.from_inner_text(
+    #     '//div[@class="tm-player-performance__statistic-gauges svelte-1jbxbl0"]//div[2]/div//span[1]')
+    #
+    # if player['position'] == 'Torwart':
+    #     player['goals_conceded'] = lookup.from_inner_text(
+    #         '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[1]/li[2]/a')
+    #     player['clean_sheets'] = lookup.from_inner_text(
+    #         '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[1]/li[3]/a')
+    #     player['penalty_saves'] = lookup.from_inner_text(
+    #         '//div[@class="tm-player-performance__statistic-gauges svelte-1jbxbl0"]//div[3]/div//span[1]')
+    #     player['goals'] = ''
+    #     player['assists'] = ''
+    #     player['goal_participation'] = ''
+    # else:
+    #     player['goals'] = lookup.from_inner_text(
+    #         '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[1]/li[2]/a')
+    #     player['assists'] = lookup.from_inner_text(
+    #         '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[1]/li[3]/a')
+    #     player['goal_participation'] = lookup.from_inner_text(
+    #         '//div[@class="tm-player-performance__statistic-gauges svelte-1jbxbl0"]//div[3]/div//span[1]')
+    #     player['goals_conceded'] = ''
+    #     player['clean_sheets'] = ''
+    #     player['penalty_saves'] = ''
 
-    player['games'] = lookup.from_inner_text(
-        '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[1]/li[1]/a')
-    player['yellow_cards'] = lookup.from_inner_text(
-        '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[2]/li[1]/a')
-    player['yellow_red_cards'] = lookup.from_inner_text(
-        '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[2]/li[2]/a')
-    player['red_cards'] = lookup.from_inner_text(
-        '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[2]/li[3]/a')
-
-    player['starting_eleven'] = lookup.from_inner_text(
-        '//div[@class="tm-player-performance__statistic-gauges svelte-1jbxbl0"]//div[1]/div//span[1]')
-    player['minutes'] = lookup.from_inner_text(
-        '//div[@class="tm-player-performance__statistic-gauges svelte-1jbxbl0"]//div[2]/div//span[1]')
-
-    if player['position'] == 'Torwart':
-        player['goals_conceded'] = lookup.from_inner_text(
-            '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[1]/li[2]/a')
-        player['clean_sheets'] = lookup.from_inner_text(
-            '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[1]/li[3]/a')
-        player['penalty_saves'] = lookup.from_inner_text(
-            '//div[@class="tm-player-performance__statistic-gauges svelte-1jbxbl0"]//div[3]/div//span[1]')
-        player['goals'] = ''
-        player['assists'] = ''
-        player['goal_participation'] = ''
-    else:
-        player['goals'] = lookup.from_inner_text(
-            '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[1]/li[2]/a')
-        player['assists'] = lookup.from_inner_text(
-            '//*[@id="svelte-performance-data"]/div/main/div/div[2]/div[2]/ul[1]/li[3]/a')
-        player['goal_participation'] = lookup.from_inner_text(
-            '//div[@class="tm-player-performance__statistic-gauges svelte-1jbxbl0"]//div[3]/div//span[1]')
-        player['goals_conceded'] = ''
-        player['clean_sheets'] = ''
-        player['penalty_saves'] = ''
-
-    instagram_link = lookup.from_attribute("//div[@class='row']/div/div[2]/div/div[2]/div/span[28]/div/a", 'href')
-    if instagram_link:
-        driver.get(instagram_link)
-        player['instagram_posts'] = lookup.from_inner_text('//main/div/header/section/ul/li[1]/button/span')
-        player['instagram_followers'] = lookup.from_attribute('//main/div/header/section/ul/li[2]/button/span', 'title')
-    else:
-        player['instagram_posts'] = ''
-        player['instagram_followers'] = ''
+    # instagram_link = lookup.from_attribute("//div[@class='row']/div/div[2]/div/div[2]/div/span[28]/div/a", 'href')
+    # if instagram_link:
+    #     driver.get(instagram_link)
+    #     player['instagram_posts'] = lookup.from_inner_text('//main/div/header/section/ul/li[1]/button/span')
+    #     player['instagram_followers'] = lookup.from_attribute('//main/div/header/section/ul/li[2]/button/span', 'title')
+    # else:
+    #     player['instagram_posts'] = ''
+    #     player['instagram_followers'] = ''
 
     # todo: features to add:
     # - erweiterte detaillierte leistungsdaten aus der vergangenen saison in der liga
     # - fifa-score
-
-    for key, value in player.items():
-        player[key] = value.replace('\n', '')
 
     return player
 
@@ -157,7 +166,7 @@ def write_player(timestamp, player):
 
 
 def log_scraping_error(timestamp, player_link, exception):
-    print(f"Error:\t\t{player_link}")
+    print(f"Error:\t\t{player_link}\t\t{str(exception)}")
     filepath = f"data/errors_{timestamp}.csv"
     with open(filepath, 'a') as f:
         f.write(player_link + '\n')
@@ -178,7 +187,8 @@ def get_driver():
     if not show_browser_ui:
         options.add_argument("--headless")
     driver = webdriver.Chrome('chromedriver.exe', options=options)
-    driver.implicitly_wait(30)
+    # todo: overriding timeout necessary (reducing timeout speeds up scraping time significantly but might lead to missing data when having a bad internet connection)
+    # driver.implicitly_wait(30)
     driver.maximize_window()
     return driver
 
@@ -188,12 +198,10 @@ class HtmlLookup:
         self._driver = driver
 
     def from_attribute(self, xpath, attribute):
-        return self._try(lambda driver: driver.find_element(By.XPATH, xpath),
-                         lambda element: element.get_attribute(attribute))
+        return self._try(lambda driver: driver.find_element(By.XPATH, xpath), lambda element: element.get_attribute(attribute))
 
     def from_inner_text(self, xpath):
-        return self._try(lambda driver: driver.find_element(By.XPATH, xpath),
-                         lambda element: element.get_attribute('innerText'))
+        return self.from_attribute(xpath, 'innerText')
 
     def from_text(self, xpath):
         return self._try(lambda driver: driver.find_element(By.XPATH, xpath), lambda element: element.text)
